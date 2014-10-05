a131c8ad97c06b3d02cf480802e73b436c99b05f
mithun <mithun3@gmail.com>
2014-10-05 20:37:51 +0530

    added OfficeController & OfficeRepository

---
+++ src/main/java/com/mithun/contacts/repositories/OfficeRepository.java
@@ -0,0 +1,9 @@
+package com.mithun.contacts.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mithun.contacts.entities.Office;
+
+public interface OfficeRepository extends JpaRepository<Office, Long>{
+
+}
